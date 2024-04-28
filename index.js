aee611c5b88ef186f40a069f8581a79aad96438e
in100tiva <projetoin100tiva@gmail.com>
2024-04-28 19:26:56 -0300

    Adicionando um menu flutuante na janela de edição
    Adicionando um menu flutuante na janela de edição

--- index.js
+++ index.js
@@ -14,19 +14,35 @@ function addItem(name, quantity) {
         <td>${name}</td>
         <td>${quantity}</td>
         <td>
-            <button class="button" onclick="editItem(this)">Editar</button>
+            <button class="button edit-button" onclick="editItem(this)">Editar</button>
             <button class="button" onclick="deleteItem(this)">Deletar</button>
         </td>
     `;
 }
 
 function editItem(btn) {
+    const modal = document.getElementById('editModal');
+    modal.style.display = 'block';
     const row = btn.parentNode.parentNode;
-    const name = prompt("Atualize o nome do item:", row.cells[0].innerHTML);
-    const quantity = prompt("Atualize a quantidade:", row.cells[1].innerHTML);
-    if (name && quantity) {
-        row.cells[0].innerHTML = name;
-        row.cells[1].innerHTML = quantity;
+    document.getElementById('editName').value = row.cells[0].innerHTML;
+    document.getElementById('editQuantity').value = row.cells[1].innerHTML;
+
+    document.getElementById('editForm').onsubmit = function (e) {
+        e.preventDefault();
+        row.cells[0].innerHTML = document.getElementById('editName').value;
+        row.cells[1].innerHTML = document.getElementById('editQuantity').value;
+        modal.style.display = 'none';
+    }
+}
+
+document.getElementsByClassName('close-button')[0].onclick = function() {
+    document.getElementById('editModal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById('editModal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
     }
 }
 
